135ab6bdef9d97dd26dc785ba09318fedeac6d37
brendan.codes <brendan_twz@hotmail.com>
2024-03-04 16:20:08 +0800

    Display TechniquesList on the main react app after integrating backend to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,16 @@ import footer from './components/Footer.js';
 import header from './components/Header.js';
 import sidebar from './components/Sidebar.js';
 import navbar from './components/Navigation.js';
+import TechniquesList from './components/TechniquesList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Jiu-jitsu Techniques</h1>
       </header>
+      <TechniquesList />
     </div>
   );
 }
